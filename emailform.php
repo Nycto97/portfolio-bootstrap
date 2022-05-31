5990815e0143467dc1a548ee6c76fe460dec5ff5
Nycto97 <jelle.van.goethem@hotmail.com>
2022-06-01 01:32:44 +0200

    Try PHP script from Matijs (in HTML file)

--- emailform.php
+++ emailform.php
@@ -2,7 +2,7 @@
 if (isset($_POST['email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "jelle.van.goethem@hotmail.com";
+    $email_to = "<m";
     $email_subject = "New form submissions";
 
     function problem($error)
