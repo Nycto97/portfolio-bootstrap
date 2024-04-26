a282755f32d8433f3a5056d6d5480c1e5025df5b
Jelle Van Goethem <jelle.van.goethem@hotmail.com>
2024-04-26 11:01:56 +0200

    Debounce window resize callback for improved performance
    Some browser events have an extremely high-frequency rate. The number of events such as cursor moving, scrolling, resizing the browser window, holding down a key, etc., can reach 100 times per second. Functions called at this frequency of events place a heavy load on the browser and it leads to a decrease in performance.
    A debounce function will delay calling a function until after the specified delay has passed since the last time it was invoked. If the debounced function is invoked again before the delay has passed, it resets the delay.

---
+++ src/scripts/navbar-utils.js
@@ -0,0 +1,83 @@
+/*
+ * Bootstrap Portfolio
+ * Portfolio website of Jelle Van Goethem.
+ * Copyright (C) 2024 Jelle Van Goethem
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Script to hide the navbar hamburger menu when a navbar hamburger menu link is clicked,
+ * or when the window gets resized in a way that would turn the hamburger menu into a normal
+ * navbar and back into a hamburger menu, while the hamburger menu was shown before resizing.
+ */
+
+import { debounce } from './utils.js';
+
+/**
+ * The Bootstrap medium breakpoint (md) in pixels.
+ * @type {number}
+ */
+const BOOTSTRAP_MEDIUM_BREAKPOINT = 768;
+const COLLAPSE = bootstrap.Collapse;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const homeLink = document.getElementById('homeLink');
+    const navMenu = document.getElementById('navMenu');
+    const navMenuCollapseInstance = COLLAPSE.getOrCreateInstance(navMenu, { toggle: false });
+    const navMenuLinks = navMenu.querySelectorAll('a');
+
+    const hideNavHamburgerMenu = () => navMenuCollapseInstance.hide();
+    const hideNavHamburgerMenuWithoutAnimation = () => navMenu.classList.remove('show');
+
+    const isNavHamburgerMenuOpen = () => navMenu.classList.contains('show');
+    const isNavbarHamburgerMenu = () => window.innerWidth < BOOTSTRAP_MEDIUM_BREAKPOINT;
+
+    const addClickListenersToNavbarLinks = () => {
+        homeLink.addEventListener('click', hideNavHamburgerMenu);
+        navMenuLinks.forEach((navMenuLink) => navMenuLink.addEventListener('click', hideNavHamburgerMenu));
+    };
+    const removeClickListenersFromNavbarLinks = () => {
+        homeLink.removeEventListener('click', hideNavHamburgerMenu);
+        navMenuLinks.forEach((navMenuLink) => navMenuLink.removeEventListener('click', hideNavHamburgerMenu));
+    };
+
+    const addCollapseListenersToNavMenu = () => {
+        navMenu.addEventListener('show.bs.collapse', addClickListenersToNavbarLinks);
+        navMenu.addEventListener('hide.bs.collapse', removeClickListenersFromNavbarLinks);
+    };
+    const removeCollapseListenersFromNavMenu = () => {
+        navMenu.removeEventListener('show.bs.collapse', addClickListenersToNavbarLinks);
+        navMenu.removeEventListener('hide.bs.collapse', removeClickListenersFromNavbarLinks);
+    };
+
+    const onWindowResize = () => {
+        if (isNavbarHamburgerMenu()) addCollapseListenersToNavMenu();
+        else {
+            removeCollapseListenersFromNavMenu();
+
+            /*
+             * Hide the navbar hamburger menu when the window width exceeds the medium (md)
+             * breakpoint (hamburger menu turns into normal navbar) while the navbar menu is
+             * shown (in the background) to prevent the navbar hamburger menu from being shown
+             * after resizing in a way that would turn the navbar back into a hamburger menu.
+             */
+            if (isNavHamburgerMenuOpen()) hideNavHamburgerMenuWithoutAnimation();
+        }
+    };
+
+    if (isNavbarHamburgerMenu()) addCollapseListenersToNavMenu();
+
+    window.addEventListener('resize', debounce(onWindowResize, 125));
+});
